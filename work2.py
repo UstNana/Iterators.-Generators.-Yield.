ff83b114c967cded3cf71de8105d5ef81797583f
UstNana <41002698+UstNana@users.noreply.github.com>
2019-09-07 14:04:22 +0300

    Create work2.py

---
+++ work2.py
@@ -0,0 +1,16 @@
+import hashlib
+import os
+
+def generate_file_md5(filename, blocksize=2**20):
+    m = hashlib.md5()
+    with open( os.path.join(filename) , "rb" ) as f:
+        while True:
+            buf = f.read(blocksize)
+            if not buf:
+                break
+            m.update( buf )
+    return m.hexdigest()
+
+if __name__ =="__main__":
+  for item in generate_file_md5("as.json"):
+    print(item)
